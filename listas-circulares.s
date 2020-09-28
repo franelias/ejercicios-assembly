a46a073f3dce401e65df2fac5a3b8092da60d88b
santiagoco1 <santiagoco2019@gmail.com>
2020-09-28 10:10:20 -0300

    Agregado comparador de strings

--- listas-circulares.s
+++ listas-circulares.s
@@ -96,99 +96,210 @@ new_element:
     final:
         jr $ra
 
-# -----------------------------------------------------------------------------------
+ # -----------------------------------------------------------------------------------
 
-# [Ver las listas]
-# Permite ver los elementos de una lista
-# Recibe en $a0 la direccion de la lista
+ # [Ver las listas]
+ # Permite ver los elementos de una lista
+ # Recibe en $a0 la direccion de la lista
 
-print_list:
-    move $t0, $a0
-    move $t1, $a0
+ print_list:
+     move $t0, $a0
+     move $t1, $a0
 
-    beq $a0, $zero, empty_list
+     beq $a0, $zero, empty_list
 
-    print:
-        li $v0, 4
-        lw $a0, 4($t0) # Muestra el string
-        syscall
+     print:
+         li $v0, 4
+         lw $a0, 4($t0) # Muestra el string
+         syscall
 
-        lw $t2, 8($t0)
-        beq $t2, $zero, empty_sublist
+         lw $t2, 8($t0)
+         beq $t2, $zero, empty_sublist
 
-        li $v0, 4
-        la $a0, bracket($0)
-        syscall
+         li $v0, 4
+         la $a0, bracket($0)
+         syscall
 
-        addi $sp, $sp, -16 # Guarda el contenido de $ra y $t0 en la pila
-        sw $ra, 0($sp)
-        sw $t0, -4($sp)
-        sw $t1, -8($sp)
-        move $a0, $t2
+         addi $sp, $sp, -12 # Guarda el contenido de $ra y $t0 en la pila
+         sw $ra, 0($sp)
+         sw $t0, -4($sp)
+         sw $t1, -8($sp)
+         move $a0, $t2
 
-        jal print_list
+         jal print_list
 
-        lw $ra, 0($sp)
-        lw $t0, -4($sp)
-        lw $t1, -8($sp)
-        addi $sp, $sp, 12
+         lw $ra, 0($sp)
+         lw $t0, -4($sp)
+         lw $t1, -8($sp)
+         addi $sp, $sp, 12
 
-        li $v0, 4
-        la $a0, bracket2($0)
-        syscall
+         li $v0, 4
+         la $a0, bracket2($0)
+         syscall
 
-    empty_sublist:
-        lw $t0, 12($t0) # Guardamos en $t0 la direc del siguiente elemento
+     empty_sublist:
+         lw $t0, 12($t0) # Guardamos en $t0 la direc del siguiente elemento
 
-    for1:
-        beq $t1, $t0, end_for1
+     for1:
+         beq $t1, $t0, end_for1
 
-        li $v0, 4
-        la $a0, comma($0)
-        syscall
+         li $v0, 4
+         la $a0, comma($0)
+         syscall
 
-        j print
+         j print
 
-    end_for1:
-        j end
+     end_for1:
+         j end
 
-    empty_list:
-        li $v0, 4
-        la $a0, empty_print
-        syscall
+     empty_list:
+         li $v0, 4
+         la $a0, empty_print
+         syscall
 
-    end:
-        jr $ra
+     end:
+         jr $ra
 
 # -----------------------------------------------------------------------------------
 
 # [Busca dentro de una lista]
 # Recibe en $a0 la direccion de una lista
-# y en $a1 el nombre del elemento buscado.
-# Devuelve en $v0, la direccion de la lista donde se encuentra el elemento
+# y en $a1 la direccion del nombre del elemento buscado.
+# Devuelve en $v0, la direccion de la lista donde se encuentra el elemento, 0 en caso contrario
 
 search:
     move $t0, $a0
+    move $t1, $a0
 
-    lw $t2, 4($t0)
+xddd:
 
-    li $v0, 4
-    la $a0, add_q($0)
-    syscall
+    beq $a0, $zero, failed
 
-    addi $sp, $sp, -4 # Guarda el contenido de $ra en la pila
+    lw $a1, 4($t0)
+
+    addi $sp, $sp, -20 # Guarda el contenido de $ra en la pila
     sw $ra, 0($sp)
+    sw $a0, -4($sp)
+    sw $a1, -8($sp)
+    sw $t0, -12($sp)
+    sw $t1, -16($sp)
 
-    move $a0, $v0
-
-    jal print_list
+    jal compare
 
     lw $ra, 0($sp)
-    addi $sp, $sp, 4 # Carga el contenido anterior de $ra
+    lw $a0, -4($sp)
+    lw $a1, -8($sp)
+    lw $t0, -12($sp)
+    lw $t1, -16($sp)
+    addi $sp, $sp, 12 # Carga el contenido anterior de $ra
 
-    li $v0, 4
-    la $a0, add_l($0)
-    syscall
+    li $t7, 1
+    move $t6, $v0
+    beq $v0, $t7, end3
+
+    lw $t2, 8($t0)
+    beq $t2, $zero, empty_sublist3
+
+    addi $sp, $sp, -16
+    sw $ra, 0($sp)
+    sw $t0, -4($sp)
+    sw $t1, -8($sp)
+    move $a0, $t2
+
+    jal search
+
+    lw $ra, 0($sp)
+    lw $t0, -4($sp)
+    lw $t1, -8($sp)
+    addi $sp, $sp, 12
+
+    empty_sublist3:
+        lw $t0, 12($t0) # Guardamos en $t0 la direc del siguiente elemento
+
+    for3:
+        beq $t1, $t0, end3
+        j xddd
+
+    success:
+        move $v0, $t6
+        j end3
+
+    failed:
+        li $v0, 0
+
+    end3:
+        jr $ra
+
+# [
+#     "Perro"
+#     [
+#         "Caniche",
+#         "Corgi"
+#     ],
+#     "Gato"
+#     [],
+#     "Ave"
+#     [
+#         "Paloma"
+#     ]
+# ]
+
+
+# USAR EN ELIMINATE
+# move $t0, $a0
+#
+# lw $t2, 4($t0)
+#
+# li $v0, 4
+# la $a0, add_q($0)
+# syscall
+#
+# addi $sp, $sp, -4 # Guarda el contenido de $ra en la pila
+# sw $ra, 0($sp)
+#
+# move $a0, $v0
+#
+# jal print_list
+#
+# lw $ra, 0($sp)
+# addi $sp, $sp, 4 # Carga el contenido anterior de $ra
+#
+# li $v0, 4
+# la $a0, add_l($0)
+# syscall
+
+
+
+# -----------------------------------------------------------------------------------
+
+# [Compara dos strings]
+# Recibe en $a0 y $a1 las direcciones de dos strings
+# Devuelve en $v0, un 1 si son iguales, 0 si no
+compare:
+    move $t2, $a0
+    move $t3, $a1
+    for2:
+        lb $t0, 0($t2)
+        lb $t1, 0($t3)
+
+        bne $t0, $t1, false
+
+        beq $t0, $zero, true
+
+        addi $t2, 1
+        addi $t3, 1
+
+        j for2
+
+    false:
+        li $v0, 0
+        j end_for2
+
+    true:
+        li $v0, 1
+
+    end_for2:
+        jr $ra
 
 
 #      f ->  1 -> 2 -> 3 -> 4 -> 1 .....
